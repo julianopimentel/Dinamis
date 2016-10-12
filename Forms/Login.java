15ea93f232fbbc0673042a60a9a39b8b41ed9ba4
Juliano-PC\Juliano P <juliano_pimentel@live.com>
2016-10-11 23:10:16 -0400

    Correção Login

--- Forms/Login.java
+++ Forms/Login.java
@@ -115,11 +115,6 @@ public class Login extends javax.swing.JFrame {
                 jButton1MouseClicked(evt);
             }
         });
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
-            }
-        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -178,13 +173,8 @@ public class Login extends javax.swing.JFrame {
         }
     }//GEN-LAST:event_btnEntrarActionPerformed
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        //Abrir janela about
-
-    }//GEN-LAST:event_jButton1ActionPerformed
-
     private void jButton1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton1MouseClicked
-        About.main(null);
+        
     }//GEN-LAST:event_jButton1MouseClicked
 
     public static void main(String args[]) {
